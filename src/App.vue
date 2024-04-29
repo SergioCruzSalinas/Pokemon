284e57aef2daa152b1d5985676d0691d92830912
SergioCruzSalinas <sergiocruz7913@gamil.com>
2024-04-29 12:01:19 -0600

    se agrega el axios

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,37 @@
-<template>
-  <v-app>
-    <v-main>
-      <HelloWorld />
-    </v-main>
+<script>
+import axios from 'axios';
 
-    <AppFooter />
-  </v-app>
-</template>
+export default {
+  data() {
+    return {
+      nombresPokemon: []
+    };
+  },
+  mounted() {
+    const instanciaAxios = axios.create({
+      baseURL: 'https://pokeapi.co/api/v2/pokemon/'
+    });
 
-<script setup>
-  //
+    for (let pokemon = 1; pokemon <= 100; pokemon++) {
+      instanciaAxios.get(`${pokemon}/`)
+        .then(response => {
+          this.nombresPokemon.push(response.data.name);
+        })
+        .catch(error => {
+          console.error('Hubo un error al mostrar el nombre:', error);
+          // Puedes mostrar un mensaje de error en la interfaz si lo deseas
+        });
+    }
+  }
+}
 </script>
+
+
+
+<template>
+  <div>
+    <div v-for="nombre in nombresPokemon" :key="nombre">
+      <h1>{{ nombre }}</h1>
+    </div>
+  </div>
+</template>
